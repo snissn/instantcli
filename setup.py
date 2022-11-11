e5d9e6b50342ff85eea2c44905cba61e140c2f66
mike seiler <snissn@gmail.com>
2022-11-10 20:32:16 -1000

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name="instantcli",
-        version="1.0.2",
+        version="1.0.3",
         install_requires=[
                 'Click',
             ]
